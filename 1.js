ddbe18b66de2f621f3d2987d2fc0714504813ca9
Sergey Maslinikov <maslinikovsergey@gmail.com>
2021-05-31 09:28:43 -0400

    seccond lesson

--- 1.js
+++ 1.js
@@ -1,10 +1,20 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 1200;
+let income = "freelance";
+let addExpenses = "Internet, Taxi, Communal";
+let deposit = true;
+let mission = 50000;
+let period = 11;
 
-alert("Hello world");
+//alert("Hello world");
 
-console.log("Good news everyone");
+console.log(typeof money, typeof income, typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} долларов`);
+
+let newExp = addExpenses.toLowerCase();
+const newArr = newExp.split();
+console.log(newArr);
+
+budgetDay = money / 30;
+console.log(budgetDay);
