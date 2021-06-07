9f2cb7f059ae9652501181734a2f3aa5f234864b
Sergey Maslinikov <maslinikovsergey@gmail.com>
2021-06-07 18:37:45 -0400

    Calc

--- script.js
+++ script.js
@@ -2,6 +2,37 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n) && isFinite(n));
 };
 
+let startEl = document.getElementById("start");
+let buttonPlusElFirst = document.getElementsByTagName("button")[0];
+let buttonPlusElSecond = document.getElementsByTagName("button")[1];
+let checkBoxEl = document.querySelector("#deposit-check");
+let additionalIncomeItemEls = document.querySelectorAll(
+  ".additional_income-item"
+);
+
+let budgetDayValue = document.getElementsByClassName("budget_day-value");
+let budgetMounthValue = document.getElementsByClassName("expenses_month-value");
+let additionalIncomeValue = document.getElementsByClassName(
+  "additional_income-value"
+);
+let additionalExpensesValue = document.getElementsByClassName(
+  "additional_expenses-value"
+);
+let incomePeriodValue = document.getElementsByClassName("income_period-value");
+let targetMonthValue = document.getElementsByClassName("target_month-value");
+
+let salaryAmount = document.querySelector(".salary-amount");
+let incomeTitle = document.querySelector(".income-title");
+let incomeAmount = document.querySelector(".income-amount");
+let expensesTitle = document.querySelector(".expenses-title");
+let expensesAmount = document.querySelector(".expenses-amount");
+let additionalExpensesItem = document.querySelector(
+  ".additional_expenses-item"
+);
+let targetAmount = document.querySelector(".target-amount");
+let periodSelect = document.querySelector(".period-select");
+let budgetMonthValue = document.querySelector(".budget_month-value");
+
 let money,
   start = function () {
     do {
